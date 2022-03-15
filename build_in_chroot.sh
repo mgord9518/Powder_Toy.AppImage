4f53cbb2cd200fa3f07d06c784ecc818217c0771
Mathew Gordon <mgord9518@gmail.com>
2022-03-15 08:31:01 +0000

    Add apt update

--- build_in_chroot.sh
+++ build_in_chroot.sh
@@ -16,6 +16,7 @@ sudo mount --rbind /run/systemd chrootdir/run/systemd
 
 # Everything below will be run inside the chroot
 cat << EOF | sudo chroot chrootdir /bin/bash
+sudo apt update
 sudo apt install libssl-dev libluajit-5.1-dev libcurl4-openssl-dev zlib1g-dev libsdl2-dev pkg-config ccache python3-pip git libfftw3-dev
 sudo pip3 install meson ninja
 
